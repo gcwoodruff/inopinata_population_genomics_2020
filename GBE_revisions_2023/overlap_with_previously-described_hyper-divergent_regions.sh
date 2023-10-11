33fd14e0300210ea0f488ad8fd91525db081faa5
gcwoodruff <49496951+gcwoodruff@users.noreply.github.com>
2023-10-11 16:24:07 -0500

    Update overlap_with_previously-described_hyper-divergent_regions.sh

--- GBE_revisions_2023/overlap_with_previously-described_hyper-divergent_regions.sh
+++ GBE_revisions_2023/overlap_with_previously-described_hyper-divergent_regions.sh
@@ -24,11 +24,18 @@ bedtools merge -i 20220216_c_elegans_divergent_regions_strain_first_three_column
 #get overlap of my vcf with the reported hyperdivergent regions with bed
 # -wa hyperdivergent -b my variants
 
+#contiguous regions
 bedtools intersect -wa -a /Users/gavin/genome/pop_gen_revisions_9-2023/20220216_c_elegans_divergent_regions_strain_first_three_columns_sort_uniq_bedtools_sort_merge.bed -b /Users/gavin/genome/pop_gen_revisions_9-2023/elegans_24_og_pseudo_rad_24_sorted_first_three_columns_merged.bed > /Users/gavin/genome/pop_gen_revisions_9-2023/hyperdivergent_regions_with_elegans_rad_sites.bed
+
+#sites
+bedtools intersect -a /Users/gavin/genome/pop_gen_revisions_9-2023/20220216_c_elegans_divergent_regions_strain_first_three_columns_sort_uniq_bedtools_sort_merge.bed -b /Users/gavin/genome/pop_gen_revisions_9-2023/elegans_24_og_pseudo_rad_24_sorted_first_three_columns.bed > /Users/gavin/genome/pop_gen_revisions_9-2023/elegans_24_og_pseudo_rad_24_sorted_first_three_columns_number_of_sites_in_regions.bed
 
 #remove duplicate rows
 
 sort /Users/gavin/genome/pop_gen_revisions_9-2023/hyperdivergent_regions_with_elegans_rad_sites.bed | uniq > /Users/gavin/genome/pop_gen_revisions_9-2023/hyperdivergent_regions_with_elegans_rad_sites_sort_uniq.bed
+
+sort /Users/gavin/genome/pop_gen_revisions_9-2023/elegans_24_og_pseudo_rad_24_sorted_first_three_columns_number_of_sites_in_regions.bed | uniq > /Users/gavin/genome/pop_gen_revisions_9-2023/elegans_24_og_pseudo_rad_24_sorted_first_three_columns_number_of_sites_in_regions_sort_uniq.bed
+
 
 #how many hyperdivergent regions?
 
@@ -41,5 +48,17 @@ wc -l hyperdivergent_regions_with_elegans_rad_sites_sort_uniq.bed
 
 #299/312 = 0.9583333
 
+#how many elegans pseudo-rad sites are in this region?
+
+wc -l /Users/gavin/genome/pop_gen_revisions_9-2023/elegans_24_og_pseudo_rad_24_sorted_first_three_columns_number_of_sites_in_regions_sort_uniq.bed
+
+#2105793
+#that's a lot!
+
+#how many sites do we have again in the pseudo-rad data??
+wc -l elegans_24_og_pseudo_rad_24_sorted.bed
+#14169530
+
+#2105793/14169530 (0.1486142 , 15%)
 
 
